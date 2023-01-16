fc183c410df377e4419cd58f6ce965a30e65aa50
Manamana <allentrobson@gmail.com>
2023-01-16 12:15:51 -0700

    api routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
 
+const {
+    getAllThoughts,
+    addThought,
+    getOneThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+  } = require("../../controllers/thoughtController");
+  
+  
+  router.route("/").get(getAllThoughts).post(addThought);
+   
+  router.route("/:thoughtId").get(getOneThought).put(updateThought).delete(deleteThought);
+    
+  router.route("/:thoughtId/reactions").post(addReaction);
+    
+  router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+  
+  
+
+
 module.exports = router;
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers,
+    addUser,
+    getOneUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(addUser);
+
+router.route('/:userId').get(getOneUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
 module.exports = router;
