067664b26aa66d81a0cae2d2df6411fb1d1b8439
Manamana <allentrobson@gmail.com>
2023-01-16 12:01:18 -0700

    controllers

---
+++ controllers/userController.js
@@ -0,0 +1,71 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+
+    getAllUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    addUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+
+
+
+    getOneUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select("-__v")
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with that id" })
+                    : res.json({
+                        user,
+                    })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+
+    deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No user with that id" })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } }))
+            .then(() => res.json({ message: "User deleted" }))
+            .catch((err) => {
+                res.status(500).json(err);
+            });
+    },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true })
+            .then((userData) =>
+                !userData
+                    ? res.status(404).json({ message: "No user with that id" })
+                    : res.json(userData))
+            .catch((err) => res.status(500).json(err));
+    },
+
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: "No friend with that id" })
+                    : res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+
+};
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const thoughtsRoutes = require('./thoughtsRoutes');
 
-
 router.use('/users', usersRoutes);
 router.use('/thoughts', thoughtsRoutes);
-
 
 module.exports = router;
